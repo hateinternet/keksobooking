152f398c1dc940e392672eafbfc0b24b2957f9f7
Feofilov Ilya <feofilov.ilya@gmail.com>
2017-02-11 22:29:51 +0500

    Инкапсуляция

--- js/check-event.js
+++ js/check-event.js
@@ -1,12 +1,14 @@
 'use strict';
 
-var ENTER_KEY_CODE = 13;
-var ESCAPE_KEY_CODE = 27;
-
-window.checkPressedEnter = function (evt) {
-  return evt.keyCode && evt.keyCode === ENTER_KEY_CODE;
-};
-
-window.checkPressedEsc = function (evt) {
-  return evt.keyCode && evt.keyCode === ESCAPE_KEY_CODE;
-};
+window.checkEvents = (function () {
+  var ENTER_KEY_CODE = 13;
+  var ESCAPE_KEY_CODE = 27;
+  return {
+    checkPressedEnter: function (evt) {
+      return evt.keyCode && evt.keyCode === ENTER_KEY_CODE;
+    },
+    checkPressedEsc: function (evt) {
+      return evt.keyCode && evt.keyCode === ESCAPE_KEY_CODE;
+    }
+  };
+})();
--- js/form.js
+++ js/form.js
@@ -1,36 +1,38 @@
 'use strict';
 
-window.initializePins();
+(function () {
+  window.initializePins();
 
-var fieldTitle = document.querySelector('#title');
-var fieldPrice = document.querySelector('#price');
-var fieldAddress = document.querySelector('#address');
+  var fieldTitle = document.querySelector('#title');
+  var fieldPrice = document.querySelector('#price');
+  var fieldAddress = document.querySelector('#address');
 
-fieldTitle.required = true;
-fieldTitle.minLength = 30;
-fieldTitle.maxLength = 100;
-fieldPrice.required = true;
-fieldPrice.min = 1000;
-fieldPrice.max = 1000000;
-fieldAddress.required = true;
+  fieldTitle.required = true;
+  fieldTitle.minLength = 30;
+  fieldTitle.maxLength = 100;
+  fieldPrice.required = true;
+  fieldPrice.min = 1000;
+  fieldPrice.max = 1000000;
+  fieldAddress.required = true;
 
-var fieldTime = document.querySelector('#time');
-var fieldTimeOut = document.querySelector('#timeout');
-var fieldRealtyType = document.querySelector('#type');
-var fieldRoomNumber = document.querySelector('#room_number');
-var fieldCapacity = document.querySelector('#capacity');
+  var fieldTime = document.querySelector('#time');
+  var fieldTimeOut = document.querySelector('#timeout');
+  var fieldRealtyType = document.querySelector('#type');
+  var fieldRoomNumber = document.querySelector('#room_number');
+  var fieldCapacity = document.querySelector('#capacity');
 
-var timeValues = ['12', '13', '14'];
-var timeOutValues = ['12', '13', '14'];
-var realtyTypeValues = ['appartment', 'shack', 'palace'];
-var realtyPriceValues = ['1000', '0', '10000'];
-var roomNumberValues = ['100', '2', '1'];
-var capacityValues = ['3', '3', '0'];
+  var timeValues = ['12', '13', '14'];
+  var timeOutValues = ['12', '13', '14'];
+  var realtyTypeValues = ['appartment', 'shack', 'palace'];
+  var realtyPriceValues = ['1000', '0', '10000'];
+  var roomNumberValues = ['100', '2', '1'];
+  var capacityValues = ['3', '3', '0'];
 
-fieldCapacity.value = '0';
+  fieldCapacity.value = '0';
 
-window.synchronizeFields(fieldTime, fieldTimeOut, timeValues, timeOutValues, 'value');
-window.synchronizeFields(fieldTimeOut, fieldTime, timeOutValues, timeValues, 'value');
-window.synchronizeFields(fieldRealtyType, fieldPrice, realtyTypeValues, realtyPriceValues, 'min');
-window.synchronizeFields(fieldRoomNumber, fieldCapacity, roomNumberValues, capacityValues, 'value');
-window.synchronizeFields(fieldCapacity, fieldRoomNumber, capacityValues, roomNumberValues, 'value');
+  window.synchronizeFields(fieldTime, fieldTimeOut, timeValues, timeOutValues, 'value');
+  window.synchronizeFields(fieldTimeOut, fieldTime, timeOutValues, timeValues, 'value');
+  window.synchronizeFields(fieldRealtyType, fieldPrice, realtyTypeValues, realtyPriceValues, 'min');
+  window.synchronizeFields(fieldRoomNumber, fieldCapacity, roomNumberValues, capacityValues, 'value');
+  window.synchronizeFields(fieldCapacity, fieldRoomNumber, capacityValues, roomNumberValues, 'value');
+})();
