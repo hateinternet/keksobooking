74c690d5d7e3d55ef86a11f6090740a78205e299
Feofilov Ilya <feofilov.ilya@gmail.com>
2017-02-26 15:08:18 +0500

    Добавил константы, toString(); изменил очистку пинов

--- js/initialize-pins.js
+++ js/initialize-pins.js
@@ -4,10 +4,15 @@ window.initializePins = function () {
   var pinMap = document.querySelector('.tokyo__pin-map');
   var activePin = null;
   var newPin;
+  var pins = [];
   var similarApartments = [];
   var filteredApartments = [];
   var URL_DATA = 'https://intensive-javascript-server-pedmyactpq.now.sh/keksobooking/data';
+  var ALL_VALUES = 'any';
+  var FIRST_PRICE_LIMIT = 10000;
+  var SECOND_PRICE_LIMIT = 50000;
 
+  var card = document.querySelector('.dialog');
   var mainPin = document.querySelector('.pin__main');
   var allFilters = document.querySelector('.tokyo__filters');
   var filterType = document.querySelector('#housing_type');
@@ -15,6 +20,13 @@ window.initializePins = function () {
   var filterRooms = document.querySelector('#housing_room-number');
   var filterGuests = document.querySelector('#housing_guests-number');
   var features = document.querySelectorAll('#housing_features input');
+
+  var clearMap = function () {
+    pins.forEach(function (element) {
+      pinMap.removeChild(element);
+    });
+    pins = [];
+  };
 
   var addHandlersPin = function (pin, pinData) {
     pin.addEventListener('click', function (evt) {
@@ -38,26 +50,27 @@ window.initializePins = function () {
   };
 
   var applyFilter = function () {
+    card.style.display = 'none';
     filteredApartments = [];
     filteredApartments = similarApartments.filter(function (element) {
-      return filterType.value === 'any' || element.offer.type === filterType.value;
+      return filterType.value === ALL_VALUES || element.offer.type === filterType.value;
     });
     filteredApartments = filteredApartments.filter(function (element) {
       switch (filterPrice.value) {
         case 'middle':
-          return element.offer.price >= 10000 && element.offer.price < 50000;
+          return element.offer.price >= FIRST_PRICE_LIMIT && element.offer.price < SECOND_PRICE_LIMIT;
         case 'low':
-          return element.offer.price < 10000;
+          return element.offer.price < FIRST_PRICE_LIMIT;
         case 'high':
-          return element.offer.price >= 50000;
+          return element.offer.price >= SECOND_PRICE_LIMIT;
       }
       return true;
     });
     filteredApartments = filteredApartments.filter(function (element) {
-      return filterRooms.value === 'any' || String(element.offer.rooms) === filterRooms.value;
+      return filterRooms.value === ALL_VALUES || element.offer.rooms.toString() === filterRooms.value;
     });
     filteredApartments = filteredApartments.filter(function (element) {
-      return filterGuests.value === 'any' || String(element.offer.guests) === filterGuests.value;
+      return filterGuests.value === ALL_VALUES || element.offer.guests.toString() === filterGuests.value;
     });
     features.forEach(function (feature) {
       if (feature.checked) {
@@ -67,11 +80,12 @@ window.initializePins = function () {
       }
     });
 
-    pinMap.innerHTML = '';
+    clearMap();
     pinMap.appendChild(mainPin);
     var countOfAppartments = filteredApartments.length;
     for (var j = 0; j < countOfAppartments; j++) {
       newPin = window.renderPin(filteredApartments[j]);
+      pins.push(newPin);
       pinMap.appendChild(newPin);
       addHandlersPin(newPin, filteredApartments[j]);
     }
