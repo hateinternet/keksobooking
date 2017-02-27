65beee8ead4bc9caa898e763a2305f6f204b32b9
Feofilov Ilya <feofilov.ilya@gmail.com>
2017-02-28 00:15:32 +0500

    Исправления по замечаниям проверяющего

--- js/synchronize-fields.js
+++ js/synchronize-fields.js
@@ -1,9 +1,14 @@
 'use strict';
 
-window.synchronizeFields = function (firstElement, secondElement, firstArray, secondArray, callback) {
-  firstElement.addEventListener('change', function () {
-    if (typeof callback === 'function') {
-      callback(secondElement, secondArray[firstArray.indexOf(firstElement['value'])]);
-    }
-  });
-};
+window.synchronizeFields = (function () {
+  var changeHandler = function (firstElement, secondElement, firstArray, secondArray, callback) {
+    firstElement.addEventListener('change', function () {
+      if (typeof callback === 'function') {
+        callback(secondElement, secondArray[firstArray.indexOf(firstElement['value'])]);
+      }
+    });
+  };
+  return function (firstElement, secondElement, firstArray, secondArray, callback) {
+    changeHandler(firstElement, secondElement, firstArray, secondArray, callback);
+  };
+})();
